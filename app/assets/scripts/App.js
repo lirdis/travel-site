e450f360d62d22add5b08e9e335d4e9d8fdd6e66
Lior Talmor <lior@liortalmor.com>
2021-03-19 14:17:47 +0200

    finish integrating React into the workflow

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,12 @@ import '../styles/style.css';
 if (module.hot) {
   module.hot.accept();
 }
+
+// React realted code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import React Components
+import MyAmazingComponent from './MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
